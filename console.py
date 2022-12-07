18fd678f472a500120d1ddb3d97798376a908e07
Matijs Behrens <matijs.behrens@spie.com>
2022-12-07 14:05:34 +0100

    added extra features

--- console.py
+++ console.py
@@ -1,3 +1,9 @@
+# @07122022^MB Added:
+#                    + option to change the style
+#                    + notice output
+#                    + info_spinnner
+
+
 import datetime
 from glob import glob
 import time
@@ -7,15 +13,26 @@ _show_error = True
 _show_warning = True
 _show_info = True
 _show_debug = True
+_show_notice = True
+
+_timestamp_width = 23
+_title_width = 10
 
 bool_as_int = lambda x : 1 if ( x ) else 0
 
 def fprint(text, EOL=True):
+    global _title_width, _timestamp_width
     timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000')
     endchar = ''
     if EOL:
         endchar = '\n\r'
-    print(f'[{timestamp: <23}] {text}\r', end=endchar,flush=True )
+    print(f'[{timestamp: <{_title_width}}] {text}\r', end=endchar,flush=True )
+
+# configure output style eg widths 
+def console_style_config(timestamp_w, title_w):
+    global _title_width, _timestamp_width
+    _timestamp_width = timestamp_w
+    _title_width = title_w
 
 # Functions to manipulate the outputs
 def hide_errors(state = False):
@@ -34,46 +51,57 @@ def hide_debug(state = False):
     global _show_debug
     _show_debug = state
 
+def hide_notice(state = False):
+    global _show_notice
+    _show_notice = state
 
 # output functions
-def error(text, EOL=True):
-    global _show_error
+def error(text, title = f"Error",EOL=True):
+    global _show_error, _title_width, _timestamp_width
     if _show_error:
         timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000')
         endchar = ''
-        title = f"Error"
+        
         if EOL:
             endchar = '\n\r'
-        print(f'[{timestamp: <23}] {Back.RED}{Fore.BLACK}{title: ^10}{Style.RESET_ALL} {text}\r', end=endchar,flush=True )
+        print(f'[{timestamp: <{_timestamp_width}}] {Back.RED}{Fore.BLACK}{title: ^{_title_width}}{Style.RESET_ALL} {text}\r', end=endchar,flush=True )
 
-def warning(text, EOL=True):
-    global _show_warning
+def warning(text, title="Warning", EOL=True):
+    global _show_warning, _title_width, _timestamp_width
     if _show_warning:
         timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000')
         endchar = ''
-        title = f"Warning"
         if EOL:
             endchar = '\n\r'
-        print(f'[{timestamp: <23}] {Back.YELLOW}{Fore.BLACK}{title: ^10}{Style.RESET_ALL} {text}\r', end=endchar,flush=True )
+        print(f'[{timestamp: <{_timestamp_width}}] {Back.YELLOW}{Fore.BLACK}{title: ^{_title_width}}{Style.RESET_ALL} {text}\r', end=endchar,flush=True )
 
-def info(text, EOL=True):
-    global _show_info
+def info(text, title = "Info", EOL=True):
+    global _show_info, _title_width, _timestamp_width
     if _show_info:
         timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000')
         endchar = ''
-        title = f"Info"
         if EOL:
             endchar = '\n\r'
-        print(f'[{timestamp: <23}] {Back.GREEN}{Fore.BLACK}{title: ^10}{Style.RESET_ALL} {text}\r', end=endchar,flush=True )
+        print(f'[{timestamp: <{_timestamp_width}}] {Back.GREEN}{Fore.BLACK}{title: ^{_title_width}}{Style.RESET_ALL} {text}\r', end=endchar,flush=True )
+
+def notice(text,title = f"Info", EOL=True):
+    global _show_notice, _title_width, _timestamp_width
+    if _show_notice:
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000')
+        endchar = ''
+        if EOL:
+            endchar = '\n\r'
+        print(f'[{timestamp: <{_timestamp_width}}] {Back.GREEN}{Fore.BLACK}{title: ^{_title_width}}{Style.RESET_ALL} {text}\r', end=endchar,flush=True )
+
 
 def debug(text,source="debug", EOL=True):
-    global _show_debug
+    global _show_debug, _title_width, _timestamp_width
     if _show_debug:
         timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000')
         endchar = ''
         if EOL:
             endchar = '\n\r'
-        print(f'[{timestamp: <23}] {Back.MAGENTA}{Fore.BLACK}{source: ^10}{Style.RESET_ALL} {text}\r', end=endchar,flush=True )
+        print(f'[{timestamp: <{_timestamp_width}}] {Back.MAGENTA}{Fore.BLACK}{source: ^{_title_width}}{Style.RESET_ALL} {text}\r', end=endchar,flush=True )
 
 
 def spinner(text, delay=0.2):
@@ -81,3 +109,14 @@ def spinner(text, delay=0.2):
     for char in spinner:
         fprint(f'{text} {char}',EOL=False)
         time.sleep(delay) 
+
+def info_spinner(text,title='info', delay=0.2, EOL=False):
+    global _show_info
+    if _show_info:
+        spinner = ['/','-','\\','|']
+        for char in spinner:
+            fprint(f'{Back.GREEN}{Fore.BLACK}{title: ^{_title_width}}{Style.RESET_ALL} {char} {text}                 ',EOL=False)
+            time.sleep(delay) 
+        fprint(f'{Back.GREEN}{Fore.BLACK}{title: ^{_title_width}}{Style.RESET_ALL} {text}                ',EOL=False)
+        if EOL:
+            print('')
